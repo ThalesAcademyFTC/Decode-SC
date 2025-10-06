46a401a0a507a4100b474eb43416e363e40af728
TheRealSkibidi <ladon.goguen28@thalesacademy.org>
2025-10-06 15:03:42 -0400

    Added AngleUnit to fix an error :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Johnny9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Johnny9.java
@@ -21,6 +21,8 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 import java.util.List;
 
